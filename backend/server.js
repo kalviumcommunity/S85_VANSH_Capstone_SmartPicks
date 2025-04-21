03dec8b25b801855a5647314c74d7a24add68c24
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-04-21 15:31:49 +0530

    created post api endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectdb = require("./db/db");
 const app = express();
+const mongoose = require('mongoose');
+const testingModel = require('./model/user');
 PORT = 3000
 
 app.use(express.json())
@@ -10,6 +12,15 @@ connectdb();
 
 app.get('/get',(req,res)=>{
     res.send("Completed the task of creating an get endpoint")
+})
+
+app.post('/post',async (req,res)=>{
+    const {name,email,password} = req.body;
+
+    const createuser = await testingModel.create({name , email , password});
+    res.send(createuser);
+
+    res.send("Completed the task of creating an post endpoint");
 })
 
 app.listen(PORT,(req,res)=>{
